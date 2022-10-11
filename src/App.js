6d600231e1b456015f9fc4df37df62bb350b7dd6
EduardoSenpai <eduarquetz@gmail.com>
2022-10-11 11:21:27 -0500

    Inicio de pruebas

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Proyecto de prueba...
+          Iniciar proyecto de prueba...
         </p>
       </header>
     </div>
