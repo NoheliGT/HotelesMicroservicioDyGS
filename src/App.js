004c68ee4011551109e200233f2dfbb0d897fec9
EduardoSenpai <eduarquetz@gmail.com>
2022-10-11 11:15:24 -0500

    mensaje de la actualizacion

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Proyecto final en proceso...
+          Proyecto de prueba...
         </p>
       </header>
     </div>
