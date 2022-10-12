2fd5cc167d098f6103bdf813574541b24709418e
EduardoSenpai <eduarquetz@gmail.com>
2022-10-12 18:03:47 -0500

    Trabajando interfaz principal

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
-  const url = 'https://www.inegi.org.mx/app/api/denue/v1/consulta/BuscarEntidad/hoteles/4/1/5/45866f16-93af-4d29-90e5-c8ec316c0509';
+  const url = 'https://www.inegi.org.mx/app/api/denue/v1/consulta/BuscarEntidad/hoteles/4/1/10/45866f16-93af-4d29-90e5-c8ec316c0509';
   const [todos, setTodos] = useState();
   const fetchApi = async () => {
   const response = await fetch(url)
@@ -16,8 +16,10 @@ function App() {
 useEffect(() =>{
 fetchApi()
 }, [])
-
+const imgcero = "https://telegra.ph/file/fb1bc5d6d26b3897cda28.jpg";
+const imguno = "https://telegra.ph/file/16370a7fa38ebf39cbc0b.jpg";
   return (
+
     <div className="App">
     <nav className="navbar navbar-dark bg-dark">
     <div className="container">
@@ -25,11 +27,51 @@ fetchApi()
     </div>
     </nav>
       <header className="App-header">
-        <p>
+      <div>
+      <ul className='padrelista'>
+        <li className='lista'>
+        <img className='imagen' src={imgcero} alt=""/>
+        <div>
+        {todos[0].Nombre}
+        {todos[0].Ubicacion}
+        {todos[0].Correo_e}
+        {todos[0].Sitio_internet}
+        </div>
+        </li>
+        <li className='lista'>
+        <img className='imagen' src={imguno} alt=""/>
+        <div>
+        {todos[1].Nombre}
+        {todos[1].Ubicacion}
+        {todos[1].Correo_e}
+        {todos[1].Sitio_internet}
+        </div>
+        </li>
+        <li className='lista'>
+        <img className='imagen' src={imguno} alt=""/>
+        <div>
+        {todos[2].Nombre}
+        {todos[2].Ubicacion}
+        {todos[2].Correo_e}
+        {todos[2].Sitio_internet}
+        </div>
+        </li>
+        <li className='lista'>
+        <img className='imagen' src={imguno} alt=""/>
+        <div>
+        {todos[3].Nombre}
+        {todos[3].Ubicacion}
+        {todos[3].Correo_e}
+        {todos[3].Sitio_internet}
+        </div>
+        </li>
+      </ul>
+      </div>
+  {/*     <p>
         Hotel:  {todos[0].Nombre} <br></br> Ubicación: {todos[0].Ubicacion}
         <br></br> Domicilio: {todos[0].Calle}, Colonia: {todos[0].Colonia} <br></br>Correo de contacto: {todos[0].Correo_e} 
         <br></br>Página oficial: {todos[0].Sitio_internet}
-        </p>
+        </p> */}
       </header>
     </div>
   );
