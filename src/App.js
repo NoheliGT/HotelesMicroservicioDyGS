decbedbcf0027f40797478e9a84f020c51240d28
NoheliGT <87887078+NoheliGT@users.noreply.github.com>
2022-11-08 09:31:07 -0600

    Desarrollo en Interfaz

--- src/App.js
+++ src/App.js
@@ -7,11 +7,15 @@ import todos from "./hoteles.json";
  import Row from 'react-bootstrap/Row';
  import Button from 'react-bootstrap/Button';
  import {useState} from "react";
+ import Offcanvas from 'react-bootstrap/Offcanvas';
 
 
 function App() {
   const [query, setQuery] = useState("")
+  const [show, setShow] = useState(false);
 
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
 
   return (
@@ -20,6 +24,17 @@ function App() {
 
 <div className='holder'>
         <header className='header'>
+        <div className='espacio'>
+        <Button className="mb-2" variant="outline-light" onClick={handleShow}>+Información</Button>
+        <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+        <Offcanvas.Title>Hotelería en Camepeche</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+        En proceso...
+        </Offcanvas.Body>
+      </Offcanvas>           
+      </div>
             <div className='header-content flex flex-c text-center text-white'>
                 <h2 className='header-title text-capitalize'>Hoteleria en Campeche</h2><br />
                 <p className='header-text fs-18 fw-3'>¡Este es Campeche, señores, la tierra del pregonero! se levanta con el sol y se oye con los luceros!</p>
@@ -27,7 +42,6 @@ function App() {
                 <input placeholder="¡Escribe un Hotel!" onChange={event => setQuery(event.target.value)}/>
                 </div>
             </div>
-            
         </header>
     </div>
     <body>
